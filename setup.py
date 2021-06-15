1c3fbaff958fbc9c48b5cee9d2540b7ea90dddf9
Gagandeep Singh Wasir <gdswasir@outlook.com>
2021-06-16 00:29:21 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'funky-ml',
+    version = '0.0.1',
+    description = 'It literally Does Everyting from Visualization to Prediction',
+    py_module = ['funky-ml'],
+    pakage_dir = {'':'src'}
+)
