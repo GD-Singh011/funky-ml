b69fc21eafc5bf7320228b95829917387e0840d5
Arsh Anwar <45912425+d4rk-lucif3r@users.noreply.github.com>
2021-06-18 17:26:08 +0530

    Added Docstring
    Changelog Added Doctring

--- funky-ml.py
+++ funky-ml.py
@@ -1,4 +1,53 @@
 def do_everything(data, hues,features,labels,test_size = 0.2,random_state =42, tune = 'n',cv_folds = 5):
+    """
+    This Function takes data, hues , features, labels as input and performs everything right from Visualisation to Prediction using 11 Models.
+    Features:
+        Visualisations
+            1) Bar plots
+            2) Box Plots
+            3) Distribution Plots
+            4) Correlation and Pairplots
+            5) 
+        Preprocessing
+            1) SMOTE 
+            2) Label Encoding and One-Hot Encoding of categorical data.
+            3) Splits data into Test and Train Set
+            4) Scaling Data
+        Prediction
+            1) Logistic Regression
+            2) Support Vector Classification
+            3) K-Nearest Neighbors
+            4) Decision Tree Classifiers
+            5) GaussianNB
+            6) Stochastic Gradient Descent
+            7) Random Forest Classifier
+            8) AdaBoost Classifier
+            9) Gradient Boosting
+            10) Light Gradient Boosting
+            11) PassivaAggressive Classifier
+            
+        CrossValidation
+            1) K-Fold CV
+            2) GridSearch CV
+        
+        Parameters:
+            data : pd.DataFrame
+                Dataset
+            hues : str
+                Hues for visualisation
+            features : pd.DataFrame
+                Features for Prediction
+            labels : pd.DataFrame
+                Labels for prediction
+            test_size : int or float
+                Percentage for test set split. Default = 0.2
+            random_state : int
+            tune : str
+                Whether to enable hypertuning or not. Default = 'n
+            cv_folds : int
+                No. of CV Folds. Default 5
+                
+    """
 
     from sklearn.model_selection import GridSearchCV,StratifiedKFold
     from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
